60f536deaa02cda9717abb554e2dfc98be0d7835
Jay Pankey <pankey.jay@gmail.com>
2022-05-01 14:51:13 -0500

    Added day number to event for tree

--- calendar/collection.js
+++ calendar/collection.js
@@ -5,21 +5,10 @@ const fs = require('fs');
 
 const apiurl = "https://georgeeliotarchive.org/api/items?collection=45";
 
-
-//newCollection = []
 axios
 .get(apiurl)
 .then(response => {
-  // create an array of contacts only with relevant data
-  //console.log("hi");
   const newCollection = response.data.map(c => {
-    // console.log(c.element_texts[0].text);
-    // console.log(c.element_texts[1].text);
-    // console.log(c.element_texts[2].text);
-    // console.log(c.element_texts[3].text);
-    //console.log(c.url);
-    // console.log(c.element_texts[5].text);
-    // console.log("------");
     return {
       id: c.id.toString(),
       url: c.url,
@@ -34,32 +23,38 @@ axios
     };
   });
   console.log("csv start");
-  const data = [];
+  const data_cal = [];
+  const data_tree = [];
   col_len = newCollection.length;
-  data.push(newCollection[col_len-1].element_texts[0].text);
+  data_cal.push(newCollection[col_len-1].element_texts[0].text);
+  data_tree.push(newCollection[col_len-1].element_texts[0].text);
   for(var i = col_len-1; i > 0; i--) {
     for(var j = 1; j < 5; j++){
-      data.push(newCollection[i].element_texts[j].text);    
+      data_cal.push(newCollection[i].element_texts[j].text);
+      data_tree.push(newCollection[i].element_texts[j].text);    
     }
-    data.push("\"" + newCollection[i].element_texts[5].text + '\"\n' + newCollection[i-1].element_texts[0].text);
+    //data_cal.push("\"" + newCollection[i].element_texts[5].text.replaceAll('"', '') + '\"\n' + newCollection[i-1].element_texts[0].text);
+      data_cal.push("\"" + newCollection[i].element_texts[5].text.replaceAll('"', '') + '\"\n' + newCollection[i-1].element_texts[0].text);
+      data_tree.push("\"" + newCollection[i].element_texts[3].text + ": " + newCollection[i].element_texts[5].text.replaceAll('"', '') + '\"\n' + newCollection[i-1].element_texts[0].text);
   }
   for(var j = 1; j < 5; j++){
-    data.push(newCollection[i].element_texts[j].text);      
+    data_cal.push(newCollection[i].element_texts[j].text); 
+    data_tree.push(newCollection[i].element_texts[j].text);     
   }
-  data.push("\"" + newCollection[i].element_texts[5].text + '\"\n');
-  console.log(data);
+  data_cal.push("\"" + newCollection[i].element_texts[5].text.replaceAll('"', '') + '\"\n');
+  data_tree.push("\"" + newCollection[i].element_texts[3].text + ": " + newCollection[i].element_texts[5].text.replaceAll('"', '') + '\"\n');
 
-  fs.writeFile("./calendar/data.csv", "Date,Year,Month,Day,Weekday,Event\n" + data.toString(), function(err) {
+  fs.writeFile("./calendar/data.csv", "Date,Year,Month,Day,Weekday,Event\n" + data_cal.toString(), function(err) {
     if(err) {
       return console.log(err);
     }
-    console.log("success");
+    console.log("success: calendar")
+  })
+  fs.writeFile("./chronology-tree/data.csv", "Date,Year,Month,Day,Weekday,Event\n" + data_tree.toString(), function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("success: tree");
   });
-  // create a new "State" object without mutating 
-  // the original State object. 
-
-
-  // store the new state object in the component's state
 })
 .catch(error => console.log(error));
-
